d44f215d837b58ca916046f6415d4b107058b497
wmayu2 <rei05sev19@gmail.com>
2020-07-18 20:48:56 +0900

    コードを更新しました。
    twitter_bot.ino を更新しました。

--- twitter_bot.ino
+++ twitter_bot.ino
@@ -7,6 +7,9 @@ Stewitter twitter("0b3601d03fd0c1a5b8ae8c6711629d86");//Twiterのトークン
 
 String last_msg;//1番最後にメンションされたツイート
 String second_msg;//2番目に最後にメンションされたツイート
+String tweet_msg;//tweetする内容
+char* tweet_ms;
+boolean tweet;//ツイートするかどうか
 
 String c_hello("@arduinoprojec16  hello");
 String c_tempe("arduinoprojec16|@arduinoprojec16  気温は？");
@@ -22,50 +25,52 @@ void setup() {
 
 void loop() {  
   unsigned long now_time = millis()-last_time;
-  if(now_time > 30000){
+  if(now_time > 60000){
     Serial.println("確認中...");
     if(twitter.lastMention()){
-      int status = twitter.wait();
+      int status = twitter.wait();  
       if(status == 200){
-        char* tweet = select_tweet(check_lastmention());
-        twitter.post(tweet);
-        Serial.println("OK!");
-      }else{ 
-        Serial.print("failed : code ");
-        Serial.println(status);
+        check_lastmention();
+        select_tweet(); 
+        tweet_ms = tweet_msg.c_str();
+        if(tweet&&twitter.post(tweet_ms)){
+          Serial.print("tweet_msg:");
+          Serial.println(tweet_msg);
+          Serial.println("OK!");
+          tweet = false;
+        }else{
+          Serial.println("スルー");//本来要らないコード
+        }
+      }else{
+                    Serial.print("failed : code ");
+            Serial.println(status);
       }
     }
     last_time = millis();
   }
 }
 
-
-String check_lastmention(){//最後にメンションされたツーイート内容が新しいかの確認
-  second_msg = last_msg;
+void check_lastmention(){//最後にメンションされたツーイート内容が新しいかの確認
   last_msg = twitter.response();//最後にメンションされたツーイート内容を取得
   if(last_msg != second_msg){
+    Serial.print("last_msg:");
     Serial.println(last_msg); 
-    return last_msg;
+    second_msg = last_msg;
+    tweet = true;
   }
-  return "0";
 }
 
-char *select_tweet(String msg){//ツイートするメッセージの決定
-  String message1 = "こんにちは！気温をツイートします。%0D分からない場合は'help'とつぶやいてください";
-  char* hello = message1.c_str();
-  String message2 = "気温はxx℃です。";
-  char* temp = message2.c_str();
-  if(msg.indexOf(c_hello) != -1){
-    Serial.println("1");
-    return hello;
+void select_tweet(){//ツイートするメッセージの決定
+  if(last_msg.indexOf(c_hello) != -1){
+    Serial.println("tweet:hello");
+    tweet_msg = "こんにちは！気温をツイートします。%0D分からない場合は'help'とつぶやいてください";
   }else if(last_msg.indexOf(c_tempe) != -1){
-    Serial.println("2");
-    return temp;
+    Serial.println("tweet:temp");
+    tweet_msg = "気温はxx℃です。";
   }else if(last_msg.indexOf(c_help) != -1){
-    Serial.println("3");
-    return "help";
+    Serial.println("tweet:help");
+    tweet_msg = "気温は?と聞くと気温をつぶやきます。";
   }else{
-    Serial.println("0");
-    return "error";
+    Serial.println("error");
   }
 }
