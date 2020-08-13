aaa46c83cabd84d1c83691831d17e812fb83c0a1
wmayu2 <rei05sev19@gmail.com>
2020-08-13 12:48:25 +0900

    twitter_bot.inoの更新

--- twitter_bot.ino
+++ twitter_bot.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Stewitter.h>
+#include <DHT.h> 
+#define DHT_PIN 8
+#define DHT_TYPE DHT11
 
 byte mac[] = { 0x--, 0x--, 0x--, 0x--, 0x--, 0x-- };//MACアドレス
 Stewitter twitter("your token");//Twiterのトークン
@@ -23,31 +26,28 @@ void setup() {
   Serial.println("program start");
 }
 
-void loop() {  
-  unsigned long now_time = millis()-last_time;
-  if(now_time > 60000){
-    Serial.println("確認中...");
-    if(twitter.lastMention()){
-      int status = twitter.wait();  
-      if(status == 200){
-        check_lastmention();
-        select_tweet(); 
-        tweet_ms = tweet_msg.c_str();
-        if(tweet&&twitter.post(tweet_ms)){
-          Serial.print("tweet_msg:");
-          Serial.println(tweet_msg);
-          Serial.println("OK!");
-          tweet = false;
-        }else{
-          Serial.println("スルー");//本来要らないコード
-        }
+void loop(){
+  Serial.println("確認中...");
+  if(twitter.lastMention()){
+    int status = twitter.wait();
+    if(status == 200){
+      check_lastmention();
+      select_tweet();
+      tweet_ms = tweet_msg.c_str();
+      if(tweet&&twitter.post(tweet_ms)){
+        Serial.print("tweet_msg:");
+        Serial.println(tweet_msg);
+        Serial.println("OK!");
+        tweet = false;
       }else{
-                    Serial.print("failed : code ");
-            Serial.println(status);
+        Serial.println("新しいメンションはありません.");
       }
+    }else{
+      Serial.print("failed : code ");
+      Serial.println(status);
     }
-    last_time = millis();
   }
+  delay(60000);
 }
 
 void check_lastmention(){//最後にメンションされたツーイート内容が新しいかの確認
@@ -65,11 +65,16 @@ void select_tweet(){//ツイートするメッセージの決定
     Serial.println("tweet:hello");
     tweet_msg = "こんにちは！気温をツイートします。%0D分からない場合は'help'とつぶやいてください";
   }else if(last_msg.indexOf(c_tempe) != -1){
+    DHT dht(DHT_PIN,DHT_TYPE);//センサの初期化
+    dht.begin();
+    delay(2000);//正確なデータを取るための時間
+    float temperature = dht.readTemperature();//温度
+    float humidity    = dht.readHumidity();//湿度
     Serial.println("tweet:temp");
-    tweet_msg = "気温はxx℃です。";
+    tweet_msg = "気温は" + String(temperature) + "℃%0D湿度は" + String(humidity) + "%です。";
   }else if(last_msg.indexOf(c_help) != -1){
     Serial.println("tweet:help");
-    tweet_msg = "気温は?と聞くと気温をつぶやきます。";
+    tweet_msg = "気温を教えてくださいと聞くと気温をつぶやきます。";
   }else{
     Serial.println("error");
   }
